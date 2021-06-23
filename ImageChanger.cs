c0964591d9364400039c89d01133b5a6082e33ed
pipaisopa <jenyaleo619828@gmail.com>
2021-06-23 12:03:59 +0300

    Create ImageChanger.cs

---
+++ ImageChanger.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageChanger : MonoBehaviour
+{
+    public Sprite NewSprite;
+    private Image img;
+    private AudioSource Audio;
+    private AudioClip Clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<Image>();
+        Audio = GetComponent<AudioSource>();
+        Audio.Play();
+    }
+
+    public void ChangeSprite()
+    {
+        img.sprite = NewSprite;
+        img.SetNativeSize();
+    }
+
+    public void ChangeColor()
+    {
+        img.color = Color.magenta;
+        
+    }
+
+
+    public void ChangAudioPlay()
+    {
+        if (Audio.isPlaying)
+        {
+            Audio.Pause();
+        }
+        else
+        {
+            Audio.Play();
+        }
+    
+    }
+
+    public void ChangeSound()
+    {
+        Audio.clip = Clip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
