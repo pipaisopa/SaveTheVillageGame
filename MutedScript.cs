7d2feb602581249e57d695be39c0477525b5f9e5
pipaisopa <jenyaleo619828@gmail.com>
2021-06-23 12:04:05 +0300

    Create MutedScript.cs

---
+++ MutedScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MutedScript : MonoBehaviour
+{
+    public AudioListener Listener;
+    private bool toggle;
+    [SerializeField] private Image MuteImg;
+    [SerializeField] private Sprite MutedImg;
+    [SerializeField] private Sprite NotMutedImg;
+
+    private void Start()
+    {
+        Listener = GetComponent<AudioListener>();
+    }
+
+    public void Mute()
+    {
+        toggle = !toggle;
+
+        if (toggle)
+        {
+            AudioListener.volume = 0f;
+            MuteImg.sprite = NotMutedImg;
+        }
+        else
+        {
+            AudioListener.volume = 1f;
+            MuteImg.sprite = MutedImg;
+        }
+            
+
+    }
+}
