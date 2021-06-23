ac261f407d950d59500397730fcb020fa5dc3fcc
pipaisopa <jenyaleo619828@gmail.com>
2021-06-23 12:07:58 +0300

    Create AudioScript.cs

---
+++ AudioScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioSource Audio;
+    void Start()
+    {
+        Audio = GetComponent<AudioSource>();
+    }
+
+    public void PlayAudio()
+    {
+        Audio.Play();
+    }
+}
