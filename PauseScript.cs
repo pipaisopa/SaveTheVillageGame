761e7a03f1cf84fdec8d85af0cf4ad7561a21f68
pipaisopa <jenyaleo619828@gmail.com>
2021-06-23 12:04:07 +0300

    Create PauseScript.cs

---
+++ PauseScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    public bool GameisPaused = false;
+    [SerializeField] private GameObject PausePanel;
+
+    public void PauseGame()
+    {
+        if (GameisPaused)
+        {
+            Resume();
+            GameisPaused = false;
+        }
+        else
+        {
+            Pause();
+            GameisPaused = true;
+        }
+    }
+
+
+    void Resume()
+    {
+        Time.timeScale = 1f;
+        PausePanel.SetActive(false);
+    }
+
+     void Pause()
+    {
+        Time.timeScale = 0f;
+        PausePanel.SetActive(true);
+    }
+}
